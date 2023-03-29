6acd154f62881fc389bcb300350004dcd8462d62
Dongyun114742164 <dongyun.lee.1@stonybrook.edu>
2023-03-29 14:54:49 +0900

    assignment 1 done

--- Homework_1.py
+++ Homework_1.py
@@ -1,5 +1,5 @@
-# Name:
-# SBUID: 
+# Name: Dongyun Lee
+# SBUID: 114742164
 
 # Remove the ellipses (...) when writing your solutions.
 
@@ -8,10 +8,20 @@
 # TODO: Complete the implementation of fahrenheit2celsius () and what_to_wear(). 
 
 def fahrenheit2celsius(fahrenheit): 
-   ...
+   return 5/9*(fahrenheit-32)
 
 def what_to_wear(celsius):
-   ...
+   if (celsius < -10):
+       print("You have to wear Puffy jacket")
+   elif (-10 <= celsius < 0):
+       print("You have to wear Scarf")
+   elif (0 <= celsius < 10):
+       print("You have to wear")
+   elif (10 <= celsius < 20):
+       print("You have to wear Light jacket")
+   else:
+       print("You have to wear T-shirt")
+    
 
 # ---------------------------- Exercise II --------------------------------------
 # ----------------- Area and perimeter of a triangle  ---------------------------
@@ -19,28 +29,60 @@ def what_to_wear(celsius):
 # compute_triangle_perimeter from scratch  
 
 def shoelace_triangle_area(x1, y1, x2, y2, x3, y3):
-    ...
+    # Computing the area of the triangle.
+    # p1 = tuple that shows (x,y) of the first point
+    # p2 = tuple that shows (x,y) of the second point
+    # p3 = tuple that shows (x,y) of the third point
+    # Return the area of the triangle.
+
+    p1 = x1, y1
+    p2 = x2, y2
+    p3 = x3, y3
+    return abs(1/2*((x1*y2 + x2*y3 + x3*y1)-(x1*y3 + x2*y1 + x3*y2)))
 
 def euclidean_distance(x1, y1, x2, y2):
-    ...
+    # Computing the euclidean distance between p1 and p2.
+    # Return the euclidean distance between p1 and p2.
 
+    import math
+    
+    p1 = x1, y1 
+    p2 = x2, y2
+    return math.sqrt((x2-x1)**2 + (y2-y1)**2)
+ 
 def compute_triangle_perimeter(x1, y1, x2, y2, x3, y3):
-    ...
+    # Computing the perimeter of the triangle with 3 vertices.
+    # Vertices = list of 3 tuples each shows (x,y) of a verice.
+    # Return the perimeter of the triangle.
 
+    p1 = x1, y1
+    p2 = x2, y2
+    p3 = x3, y3
+
+    s1 = euclidean_distance(x1, y1, x2, y2)
+    s2 = euclidean_distance(x2, y2, x3, y3)
+    s3 = euclidean_distance(x1, y1, x3, y3)
+    return s1 + s2 + s3
+ 
 
 # ---------------------------- Exercise III -------------------------------------
 # ----------------- Compute the area of a regular polygon -----------------------
 # TODO: Fill the functions deg2rad, apothem  and polygon_area 
 
+import math
 
 def deg2rad(deg):
-    ...
-
+    return deg*(math.pi/180)
+    
 def apothem(number_sides, length_side):
-   ...
+    n = number_sides
+    s = length_side
+    return length_side/(2*math.tan(math.pi/number_sides))
 
 def polygon_area(number_sides, length_side):
-   ...
+    n = number_sides
+    s = length_side
+    return (number_sides*(length_side**2))/(4*math.tan(math.pi/number_sides))
 
 
 # ---------------------------- Test -------------------------------------
@@ -61,4 +103,3 @@ print("The area of the triangle is : " + str(area) + " , its perimeter is : " + 
 number_sides = 5
 length_side = 4
 print ("The area of the polygon is : " + str(polygon_area(number_sides, length_side)))
-
